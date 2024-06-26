96ad0b3c970fa3d8602bee4ee8849be622382c37
kaushik980 <142711079+kaushik980@users.noreply.github.com>
2024-06-26 23:49:31 +0530

    Delete javascript directory

--- javascript/add.js
+++
@@ -1,8 +0,0 @@
-const x = document.querySelector("#firstnumber")
-const y = document.querySelector("#secondnumber")
-const btn = document.querySelector("#btn")
-const result = document.querySelector("#result")
-function sum (){
-    sum = parseInt( x.value) + parseInt( y.value)
-    result.innerHTML = "Sum of these number is :" +sum;
-}
